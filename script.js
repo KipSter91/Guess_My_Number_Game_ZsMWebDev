6c663f737db59cf24ab420173a878f638e533a3b
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-10-29 16:54:36 +0100

    create variables from html DOM

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const randomNumber = Math.floor(Math.random() * 21);
+console.log(randomNumber);
+
+const checkButton = document.querySelector('.check');
+console.log(checkButton);
+
+const againButton = document.querySelector('.again');
+console.log(againButton);
+
+let guessedNumber = document.querySelector('.number').textContent;
+console.log(guessedNumber);
+
+let appMessage = document.querySelector('.message').textContent;
+console.log(appMessage);
+
+let myNumber = document.querySelector('.guess').value;
+console.log(myNumber);
+
+let currentScore = document
+  .querySelector('.label-score')
+  .querySelector('.score').textContent;
+console.log(currentScore);
+
+let highScore = document
+  .querySelector('.label-highscore')
+  .querySelector('.highscore').textContent;
+console.log(highScore);

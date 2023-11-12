fe88844f213657a62a45aba48bd9349247d4cbc8
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-11-12 15:53:43 +0100

    refactoring code

--- script.js
+++ script.js
@@ -4,43 +4,34 @@ let randomNumber = Math.floor(Math.random() * 21);
 let score = 20;
 console.log(randomNumber);
 
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
 const checkNumber = () => {
   let myNumber = document.querySelector('.guess').value;
 
-  let currentScore = document
-    .querySelector('.label-score')
-    .querySelector('.score').textContent;
+  let currentScore = document.querySelector('.score').textContent;
 
   if (myNumber < 0 || myNumber > 20) {
     document.querySelector('.message').textContent = 'Invalid guess!';
-  } else {
-    if (myNumber < randomNumber) {
-      document.querySelector('.message').textContent = 'Too low...';
-      currentScore = currentScore - 1;
-      document
-        .querySelector('.label-score')
-        .querySelector('.score').textContent = currentScore;
-    }
+  }
 
-    if (myNumber > randomNumber) {
-      document.querySelector('.message').textContent = 'Too high...';
-      currentScore = currentScore - 1;
-      document
-        .querySelector('.label-score')
-        .querySelector('.score').textContent = currentScore;
-    }
-
-    if (myNumber == randomNumber) {
-      document.querySelector('.message').textContent = 'Correct number!';
-      document
-        .querySelector('.label-score')
-        .querySelector('.score').textContent = currentScore;
-      document.querySelector('.number').textContent = randomNumber;
-      document
-        .querySelector('.label-highscore')
-        .querySelector('.highscore').textContent = currentScore;
-      document.querySelector('.check').style.display = 'none';
-      document.body.style.backgroundColor = 'green';
+  if (myNumber == randomNumber) {
+    displayMessage('Correct number!');
+    document.querySelector('.score').textContent = currentScore;
+    document.querySelector('.number').textContent = randomNumber;
+    document.querySelector('.highscore').textContent = currentScore;
+    document.querySelector('.check').style.display = 'none';
+    document.body.style.backgroundColor = 'green';
+  } else if (myNumber !== randomNumber) {
+    if (currentScore > 1) {
+      displayMessage(myNumber > randomNumber ? 'Too high' : 'Too low');
+      currentScore--;
+      document.querySelector('.score').textContent = currentScore;
+    } else {
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
     }
   }
 };
@@ -49,10 +40,9 @@ const playAgain = () => {
   randomNumber = Math.floor(Math.random() * 21);
   console.log(randomNumber);
   document.body.style.backgroundColor = '#222';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.check').style.display = 'block';
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.label-score').querySelector('.score').textContent =
-    score;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
 };
